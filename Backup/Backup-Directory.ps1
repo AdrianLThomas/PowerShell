436ca9b5725c54fcdcf85f09b765ff3cd8e972c5
Adrian L Thomas <adrian.thomas@gmail.com>
2015-08-30 15:09:43 +0100

    Added backup and setup scripts

---
+++ Backup/Backup-Directory.ps1
@@ -0,0 +1,21 @@
+Param(
+    [string]$7zLocation,
+    [string]$backupSource,
+    [string]$backupDestination,
+    [string]$backupName,
+    [string]$password
+)
+
+$filename = Get-Date -format yyyy_M_dd.HHmm
+
+if ($7zLocation){
+    Write-Host "Backing up $backupName..."
+
+    & $7zLocation a "$backupDestination\$backupName\$filename.7z" -p"$password" -r -v10m $backupSource\*.*
+
+    if($LASTEXITCODE -ne 0){
+        Write-Error "There was an error creating the archive (7-ZIP error code $LASTERRORCODE)"
+    }
+}else{
+    Write-Error "7-Zip application not provided"
+}
---
+++ Backup/Backup-Master.ps1
@@ -0,0 +1,24 @@
+$7zip = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | ?{$_.DisplayName -like "7-Zip*"}
+$7zLocation = "C:\Program Files\7-Zip\7z.exe"
+$backupDestination = "E:\Backups"
+$backupPersonalSource = "D:\Personal"
+
+if($7zip){
+    Write-Verbose "7-Zip found"
+    Write-Host "*** Adrian's Master Backup Script ***"
+    Write-Host "Please enter a password to use for encryption"
+    $password = Read-Host -AsSecureString
+
+    $startTime = Get-Date
+
+    & .\Backup-Directory.ps1 $7zLocation $backupPersonalSource $backupDestination "Personal" $password
+
+    $endTime = Get-Date
+
+    Write-Host "Master backup complete" -ForegroundColor Green
+    Write-Host "Started: $startTime. Ended: $endTime" -ForegroundColor Yellow
+    Pause
+}else{
+    Write-Error "7-Zip not found"
+    return
+}
