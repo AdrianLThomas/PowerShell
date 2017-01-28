8836706250f13eb207a614f24a5e3f5dbf150076
Adrian L Thomas <adrian.thomas@gmail.com>
2017-01-28 17:42:47 +0000

    Modified setup script to add keys and repos so that further apps can be automatically installed

--- Setup/Ubuntu-Setup.sh
+++ Setup/Ubuntu-Setup.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
-$APPS="pinta steam-launcher p7zip mpv virtualbox redshift redshift-gtk"
 
-apt-get update
-apt-get install $APPS
+# Add Keys
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+
+# Add Repositories
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# Install Apps
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+sudo apt-get -y install \
+	git \
+	google-chrome-stable \
+	mpv \
+	pinta \
+	redshift \
+	redshift-gtk \
+	spotify-client \
+	virtualbox
 
 # To automate at a later date:
 # nVidia Drivers
 # CrashPlan - Need to configure engine start https://support.code42.com/CrashPlan/4/Troubleshooting/Stopping_And_Starting_The_CrashPlan_Service
-# Chrome
-# Flux
-# Spotify - https://www.spotify.com/uk/download/linux/
-## Then need to apply this fix: https://www.reddit.com/r/spotify/comments/3cear9/linux_spotify_scaling/
+# Spotify - apply this fix: https://www.reddit.com/r/spotify/comments/3cear9/linux_spotify_scaling/
 # VS Code - https://code.visualstudio.com/docs/?dv=linux64_deb
+# Steam-Launcher
