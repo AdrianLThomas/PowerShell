d8db4ffc9ff253fd1f5495d62da7f161742eda54
Adrian L Thomas <adrian.thomas@gmail.com>
2017-01-29 21:13:37 +0000

    Added docker to setup script. Also added check around VS code install.

--- Setup/Ubuntu-Setup.sh
+++ Setup/Ubuntu-Setup.sh
@@ -3,15 +3,18 @@
 # Add Keys
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D # Docker
 
 # Add Repositories
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 
 # Install Apps
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
 sudo apt-get -y install \
+	docker-engine \
 	git \
 	google-chrome-stable \
 	mpv \
@@ -24,13 +27,16 @@ sudo apt-get -y install \
 
 # None apt-get installs
 # VS Code
-wget http://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/vscode.deb
-sudo dpkg -i /tmp/vscode.deb
-sudo apt-get install -f -y
+if ! hash code 2>/dev/null; then
+	wget http://go.microsoft.com/fwlink/?LinkID=760868 -O /tmp/vscode.deb
+	sudo dpkg -i /tmp/vscode.deb
+	sudo apt-get install -f -y
+fi
+
+# Configuration
+sudo usermod -aG docker $(whoami) # Stop requiring sudo for docker
 
 # To automate at a later date:
 # nVidia Drivers
 # CrashPlan - Need to configure engine start https://support.code42.com/CrashPlan/4/Troubleshooting/Stopping_And_Starting_The_CrashPlan_Service
 # Spotify - apply this fix: https://www.reddit.com/r/spotify/comments/3cear9/linux_spotify_scaling/
-# VS Code - https://code.visualstudio.com/docs/?dv=linux64_deb
-# Steam-Launcher
