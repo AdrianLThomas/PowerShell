1ebc42041923e1acf7bc6de301ab5f41ddd99fee
Adrian L Thomas <AdrianLThomas@users.noreply.github.com>
2018-11-22 18:52:15 +0000

    Update Windows-Setup.ps1
    Modernised for gaming / less windows dev

--- Setup/Windows-Setup.ps1
+++ Setup/Windows-Setup.ps1
@@ -10,56 +10,37 @@ $installCommands = #BEGIN
 '
                      # "Utils"
                       choco install -y avirafreeantivirus
+                     ;choco install -y backblaze
                      ;choco install -y malwarebytes
-                     ;choco install -y GoogleChrome
+                     ;choco install -y Firefox
                      ;choco install -y 7zip
-                     ;choco install -y btsync
                      ;choco install -y ccleaner
-                     ;choco install -y imgburn
                      ;choco install -y speccy
                      ;choco install -y putty
                      ;choco install -y paint.net
                      ;choco install -y cpu-z
-                     ;choco install -y everything
-                     ;choco install -y f.lux
-                     ;choco install -y tor-browser
                      ;choco install -y treesizefree
-                     ;choco install -y truecrypt
-                     ;choco install -y vuzeclient
-                     ;choco install -y lastpass
-                     ;Start-Process "https://soundswitch.codeplex.com/"
+                     ;choco install -y vmware-workstation-player
+                     ;choco install -y speedfan
 
                      # "Audio/Video"
                      ;choco install -y foobar2000
                      ;choco install -y vlc
                      ;choco install -y spotify
 
-                     # "Dev"
-                     ;choco install -y visualstudiocode
-                     ;choco install -y fiddler4
-                     ;choco install -y sysinternals
-                     ;choco install -y git.install
-
                      # "Drivers"
                      ;choco install -y geforce-experience
 
                      # "Gaming"
-                     ;choco install -y steam -y
-                     ;choco install -y origin -y
-                     ;choco install -y battle.net -y
+                     ;choco install -y steam
+                     ;choco install -y origin
+                     ;choco install -y battle.net
                      ;Start-Process "http://support.logitech.com/software/gaming-software" #For the G27
-                     ;Start-Process "https://www.gog.com/galaxy"
+                     ;choco install -y goggalaxy
                      ;Start-Process "https://www3.oculus.com/en-us/setup/"
 
-                     # "Misc"
-                     ;choco install -y Fitbit.Connect
-                     ;choco install -y skype
 '
 #END
-
-$manualInstalls = "Visual Studio (MSDN)",
-                  "Microsoft Office (MSDN)"
-
 
 Write-Host "Press enter to begin or Ctrl+C to cancel"
 Read-Host
@@ -69,15 +50,5 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 foreach($command in $installCommands){
     Invoke-Expression $command
 }
-
-Write-Host -ForegroundColor Yellow "The following needs installing manually:"
-foreach($installs in $manualInstalls){
-    Write-Host -ForegroundColor Yellow "- " $installs
-}
-
-Write-Host "Installing customisations"
-Write-Host "Customising command line colour palette"
-git clone https://github.com/neilpa/cmd-colors-solarized %temp%\cmd-colors-solarized
-regedit /s %temp%\cmd-colors-solarized\solarized-dark.reg
 
 Write-Host -ForegroundColor Green "=== Setup Complete! ==="
